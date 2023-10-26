26adb58aefb0ad49186ebc48887e3481438ab5b7
chryd <106443732+chryd@users.noreply.github.com>
2023-10-26 14:55:54 -0400

    Creation des classes

---
+++ ChatServer/src/com/chat/serveur/Invitation.java
@@ -0,0 +1,6 @@
+package com.chat.serveur;
+
+public class Invitation {
+    String hostAlias;
+    String guestAlias;
+}
---
+++ ChatServer/src/com/chat/serveur/SalonPrive.java
@@ -0,0 +1,6 @@
+package com.chat.serveur;
+
+public class SalonPrive {
+    String hostAlias;
+    String guestAlias;
+}
