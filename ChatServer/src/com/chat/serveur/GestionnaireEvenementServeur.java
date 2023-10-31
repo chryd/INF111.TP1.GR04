614dd31c15140560808f1049813eccbfa0c1bf73
chryd <106443732+chryd@users.noreply.github.com>
2023-10-31 14:22:55 -0400

    draft1 q3
    question completer, outre les messages

--- ChatServer/src/com/chat/serveur/GestionnaireEvenementServeur.java
+++ ChatServer/src/com/chat/serveur/GestionnaireEvenementServeur.java
@@ -4,6 +4,11 @@ import com.chat.commun.evenement.Evenement;
 import com.chat.commun.evenement.GestionnaireEvenement;
 import com.chat.commun.net.Connexion;
 
+<<<<<<< Updated upstream
+=======
+import java.util.*;
+
+>>>>>>> Stashed changes
 /**
  * Cette classe représente un gestionnaire d'événement d'un serveur. Lorsqu'un serveur reçoit un texte d'un client,
  * il crée un événement à partir du texte reçu et alerte ce gestionnaire qui réagit en gérant l'événement.
@@ -14,6 +19,11 @@ import com.chat.commun.net.Connexion;
  */
 public class GestionnaireEvenementServeur implements GestionnaireEvenement {
     private Serveur serveur;
+<<<<<<< Updated upstream
+=======
+    private ArrayList<Invitation> invitationList;
+    private ArrayList<SalonPrive> privateList;
+>>>>>>> Stashed changes
 
     /**
      * Construit un gestionnaire d'événements pour un serveur.
@@ -33,7 +43,11 @@ public class GestionnaireEvenementServeur implements GestionnaireEvenement {
     public void traiter(Evenement evenement) {
         Object source = evenement.getSource();
         Connexion cnx;
+<<<<<<< Updated upstream
         String msg, typeEvenement, aliasExpediteur;
+=======
+        String msg, typeEvenement, aliasExpediteur, aliasArgs;
+>>>>>>> Stashed changes
         ServeurChat serveur = (ServeurChat) this.serveur;
 
         if (source instanceof Connexion) {
@@ -50,7 +64,77 @@ public class GestionnaireEvenementServeur implements GestionnaireEvenement {
                     cnx.envoyer("LIST " + serveur.list());
                     break;
 
+<<<<<<< Updated upstream
                 //Ajoutez ici d’autres case pour gérer d’autres commandes.
+=======
+                case "JOIN": //Inviter une personne aliasArgs à chatter en privé ou d'accepter l'invitation qui lui a été envoyer par aliasArgs
+
+                    //Extraire les alias
+                    aliasExpediteur = cnx.getAlias();
+                    aliasArgs = evenement.getArgument();
+                    Invitation invitation = new Invitation(aliasExpediteur, aliasArgs);
+
+                    //Si une invitation entre ces utilisateurs a deja ete lance
+                    if (invitationList.contains(invitation)){
+                        SalonPrive salonPrive = new SalonPrive(aliasExpediteur, aliasArgs); //ouvrir un nouveau salon prive pour ces utilisateurs
+                        privateList.add(salonPrive);
+                        invitationList.remove(invitation);
+                    } else { //sinon lancer une invitation
+                        cnx.envoyer("Invitation sent to" + aliasArgs);
+                        invitationList.add(invitation);
+                    }
+                    break;
+
+                case "DECLINE":
+                    //Extraire les alias
+                    aliasExpediteur = cnx.getAlias();
+                    aliasArgs = evenement.getArgument();
+                    Invitation invite = new Invitation(aliasArgs, aliasExpediteur);
+
+                    if(invitationList.contains(invite)){
+                        if (!invite.getAliasEmetteur().equals(aliasExpediteur)){ //si l'invitation ne vient pas de l'expediteur
+                            msg = aliasArgs + "decline votre invitation";
+                            //envoie le message a l'expediteur
+                        }
+                        invitationList.remove(invite);
+                    }
+                    break;
+
+                case "INV":
+                    aliasExpediteur = cnx.getAlias();
+                    cnx.envoyer("Invitations:\n");
+                    for(Invitation i:invitationList) {
+                        String sender = i.getAliasRecepteur();
+                        if (!sender.equals(aliasExpediteur)) {
+                            cnx.envoyer(sender + "\n");
+                        }
+                    }
+                    break;
+
+                case "PRV":
+                    aliasExpediteur = cnx.getAlias();
+                    String[] args = evenement.getArgument().split(" ");
+                    aliasArgs = args[0];
+                    msg = args[1];
+                    SalonPrive salonPrive = new SalonPrive(aliasExpediteur, aliasArgs);
+
+                    if (privateList.contains(salonPrive)){
+                        //envoyer le message prive
+                    }
+                    break;
+
+                case "QUIT":
+                    aliasExpediteur = cnx.getAlias();
+                    String[] newArgs = evenement.getArgument().split(" ");
+                    aliasArgs = newArgs[0];
+                    msg = newArgs[1];
+                    SalonPrive tempSalonPrive = new SalonPrive(aliasExpediteur, aliasArgs);
+
+                    if (privateList.contains(tempSalonPrive)){
+                        privateList.remove(tempSalonPrive);
+                    }
+                    break;
+>>>>>>> Stashed changes
 
                 default: //Renvoyer le texte recu convertit en majuscules :
                     msg = (evenement.getType() + " " + evenement.getArgument()).toUpperCase();
--- ChatServer/src/com/chat/serveur/Invitation.java
+++ ChatServer/src/com/chat/serveur/Invitation.java
@@ -1,6 +1,33 @@
 package com.chat.serveur;
 
 public class Invitation {
+<<<<<<< Updated upstream
     String hostAlias;
     String guestAlias;
+=======
+    private String aliasEmetteur;
+    private String aliasRecepteur;
+
+    public Invitation(String aliasEmetteur, String aliasRecepteur){
+        this.aliasEmetteur = aliasEmetteur;
+        this.aliasRecepteur = aliasRecepteur;
+    }
+
+    public String getAliasEmetteur() {
+        return aliasEmetteur;
+    }
+
+    public String getAliasRecepteur() {
+        return aliasRecepteur;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Invitation that = (Invitation) o;
+        return aliasEmetteur.equals(that.aliasEmetteur) && aliasRecepteur.equals(that.aliasRecepteur) ||
+                aliasEmetteur.equals(that.aliasRecepteur) && aliasRecepteur.equals(that.aliasEmetteur);
+    }
+>>>>>>> Stashed changes
 }
