1b101fa166fd61a87131461fdeaf16dbd862a205
chryd <106443732+chryd@users.noreply.github.com>
2023-11-28 09:46:06 -0500

    bonne version des pieces

--- JeuEchecs/src/com/echecs/pieces/Dame.java
+++ JeuEchecs/src/com/echecs/pieces/Dame.java
@@ -1,6 +1,7 @@
 package com.echecs.pieces;
 
 import com.echecs.*;
+import com.echecs.util.*;
 
 public class Dame extends Piece {
     public Dame(char couleur) {
@@ -8,58 +9,75 @@ public class Dame extends Piece {
     }
 
     @Override
-    public boolean peutSeDeplacer(Position pos1, Position pos2, Piece[][] echiquier) {
+    public boolean peutSeDeplacer(Position positionInitiale, Position positionFinale, Piece[][] echiquier) {
 
-        //passer de la notation classique d'un echiquier aux valeurs dans la matrice
-        int colonneInitiale = pos1.getColonne()-97;
-        int ligneInitiale = -(pos1.getLigne() - 8);
-        int colonneFinale = pos2.getColonne()-97;
-        int ligneFinale = -(pos2.getLigne() - 8);
-
-        //determiner les coordonnes limite de l'iteration des case
-        int colonneStart = Math.min(colonneInitiale, colonneFinale);
-        int colonneEnd = Math.max(colonneInitiale, colonneFinale);
-
-        int ligneStart = Math.min(ligneInitiale, ligneFinale);
-        int ligneEnd = Math.max(ligneInitiale, ligneFinale);
+        //S'il y a une piece de la meme couleur sur la position finale
+        if (estLaMemeCouleur(positionInitiale, positionFinale, echiquier)){return false;}
 
         //valeur a retourner
         boolean output;
 
+        //passer de la notation classique d'un echiquier aux valeurs dans la matrice
+        int colonneInitiale = EchecsUtil.indiceColonne(positionInitiale);
+        int ligneInitiale = EchecsUtil.indiceLigne(positionInitiale);
+        int colonneFinale = EchecsUtil.indiceColonne(positionFinale);
+        int ligneFinale = EchecsUtil.indiceLigne(positionFinale);
+
         //Si la position 2 est sur le meme diagonale que la position 1
-        if (pos2.estSurLaMemeDiagonaleQue(pos1)) {
-            //verifier pour chaque case (autre qu'initial ou final) qu'elle est vide
-            for (int i = colonneStart; i < colonneEnd; i++) {
-                for (int j = ligneStart; j < ligneEnd; j++) {
-                    if (!echiquierEstVideA(i, j, echiquier)) {
-                        return false;
-                    }
+        if (positionFinale.estSurLaMemeDiagonaleQue(positionInitiale)) {
+
+            //determiner les coordonnes limite de l'iteration des case en diagonal
+            int deltaLigne = Integer.compare(colonneFinale, colonneInitiale);
+            int deltaColonne = Integer.compare(ligneFinale, ligneInitiale);
+            colonneInitiale += deltaLigne;
+            ligneInitiale += deltaColonne;
+
+            while (colonneInitiale != colonneFinale || ligneInitiale != ligneFinale){
+                if (!echiquierEstVideA(colonneInitiale, ligneInitiale,echiquier)){
+                    return false;
                 }
+                colonneInitiale += deltaLigne;
+                ligneInitiale += deltaColonne;
+
             }
             //si le chemin est libre, le deplacement est valide
             output = true;
 
             //Si la position 2 est sur la meme colonne que la position 1
-        } else if (pos2.estSurLaMemeColonneQue(pos1)) {
+        } else if (positionFinale.estSurLaMemeColonneQue(positionInitiale)) {
+            //determiner les coordonnes limite de l'iteration des cases en colonne
+            int ligneStart = Math.min(ligneInitiale, ligneFinale);
+            int ligneEnd = Math.max(ligneInitiale, ligneFinale);
+
             //verifier pour chaque case (autre qu'initial ou final) qu'elle est vide
             for (int i = ligneStart + 1; i < ligneEnd; i++) {
                 if (!echiquierEstVideA(colonneInitiale, i, echiquier)) {
                     return false;
                 }
             }
+
             output = true;
+
             //Si la position 2 est sur la meme ligne que la position 1
-        } else if (pos2.estSurLaMemeLigneQue(pos1)) {
+        } else if (positionFinale.estSurLaMemeLigneQue(positionInitiale)) {
+            //determiner les coordonnes limite de l'iteration des cases en ligne
+            int colonneStart = Math.min(colonneInitiale, colonneFinale);
+            int colonneEnd = Math.max(colonneInitiale, colonneFinale);
+
             //verifier pour chaque case (autre qu'initial ou final) qu'elle est vide
             for (int i = colonneStart + 1; i < colonneEnd; i++) {
                 if (!echiquierEstVideA(i, ligneInitiale, echiquier)) {
                     return false;
                 }
             }
-            output =  true;
+
+            output = true;
+
         } else {
+
             output = false;
         }
+
         return output;
     }
 }
--- JeuEchecs/src/com/echecs/pieces/Piece.java
+++ JeuEchecs/src/com/echecs/pieces/Piece.java
@@ -27,7 +27,7 @@ public abstract class Piece {
      * @param echiquier Piece[][] Échiquier contenant les pièces d'une partie d'échecs
      * @return boolean true, si la pièce peut se déplacer de la position pos1 à la position pos2, false sinon
      */
-    public abstract boolean peutSeDeplacer(Position pos1, Position pos2, Piece echiquier[][]);
+    public abstract boolean peutSeDeplacer(Position pos1, Position pos2, Piece[][] echiquier);
 
     /**
      *
@@ -38,5 +38,28 @@ public abstract class Piece {
      */
     protected static boolean echiquierEstVideA(int colonne, int ligne, Piece[][] echiquier){
         return echiquier[colonne][ligne] == null;
+    }
+
+    protected boolean estLaMemeCouleur(Position positionInitiale, Position positionFinale, Piece[][] echiquier){
+        //passer de la notation classique d'un echiquier aux valeurs dans la matrice
+        int colonneInitiale = positionInitiale.getColonne()-97;
+        int ligneInitiale = -(positionInitiale.getLigne() - 8);
+
+        int colonneFinale = positionFinale.getColonne()-97;
+        int ligneFinale = -(positionFinale.getLigne() - 8);
+
+        Piece pieceInitiale = echiquier[colonneInitiale][ligneInitiale];
+        Piece pieceFinale = echiquier[colonneFinale][ligneFinale];
+
+        if(echiquierEstVideA(colonneFinale, ligneFinale, echiquier)) {
+            return false;
+        }
+
+        return pieceFinale.getCouleur() == pieceInitiale.getCouleur();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.getClass()) + this.couleur;
     }
 }
